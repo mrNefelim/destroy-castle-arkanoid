64c9313533546bb2a835142edf1cfbb620947ec2
Mr Nefelim <mr.nefelim@yandex.ru>
2016-12-12 00:20:00 +0300

    menu added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	public string level;
+	public GameObject main;
+	public GameObject setting;
+	public void newGame()
+	{
+		SceneManager.LoadScene ("Level1", LoadSceneMode.Single);
+	}
+	public void resumeGame()
+	{
+		SceneManager.LoadScene (level, LoadSceneMode.Single);
+	}
+	public void settings()
+	{
+		this.gameObject.SetActive (false);
+		setting.SetActive (true);
+	}
+	public void ToMainMenu()
+	{
+		this.gameObject.SetActive (false);
+		main.SetActive (true);
+	}
+	public void soundToggle()
+	{
+		//Camera.main.GetComponent<AudioListener>.volume = 0;
+		if (AudioListener.pause == false) {
+			AudioListener.pause = true;
+		} else {
+			AudioListener.pause = false;
+		}
+	}
+	public void exit()
+	{
+		Application.Quit ();
+	}
+	// Use this for initialization
+	void Start () {
+		setting.SetActive (false);
+	}
+}
